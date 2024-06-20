7ecbaf0701bb0cfb9d89c6c89c1b870941f663e0
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-20 03:37:58 +0100

    fix: remonve if __name__...

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 """function to list all documents in a collection"""
 
-if __name__ == "__main__":
-    def list_all(mongo_collection):
-        """ lists all documents in a collection"
-        Args:
-            mongo_collection: pymongo Collection object
+def list_all(mongo_collection):
+    """ lists all documents in a collection"
+    Args:
+        mongo_collection: pymongo Collection object
 
-        Returns:
-            list of all documents
-        """
-        doc_list = []
-        if mongo_collection is None:
-            return doc_list
-        for doc in mongo_collection.find():
-            doc_list.append(doc)
+    Returns:
+        list of all documents
+    """
+    doc_list = []
+    if mongo_collection is None:
         return doc_list
+    for doc in mongo_collection.find():
+        doc_list.append(doc)
+    return doc_list
