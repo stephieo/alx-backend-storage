4760f687fbe351ce954bf5b11612545d13a30019
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-20 03:24:49 +0100

    fix: included conditional for execution

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,12 +2,13 @@
 """ function to update fields in a document """
 
 
-def update_topics(mongo_collection, name, topics):
-    """ changes all topics in a document based on the name
-    Args:
-        mongo_collection: pymongo collection object
-        name(string): school name to update
-        topics(list): list of topics approached in school
-    """
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
-    
+if __name__ == "__main__":
+    def update_topics(mongo_collection, name, topics):
+        """ changes all topics in a document based on the name
+        Args:
+            mongo_collection: pymongo collection object
+            name(string): school name to update
+            topics(list): list of topics approached in school
+        """
+        mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+        
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ reads data from document """
 
-
-def schools_by_topic(mongo_collection, topic):
-    """ return a list of schools having a specific topic
-    Args: 
-        mongo_collection: pymongo collection object
-        topic(string): topic to match
-    """
-    match_list = mongo_collection.find({"topics": topic})
-    for doc in match_list:
-        
+    if __name__ == "__main__":
+    def schools_by_topic(mongo_collection, topic):
+        """ return a list of schools having a specific topic
+        Args: 
+            mongo_collection: pymongo collection object
+            topic(string): topic to match
+        """
+        match_list = mongo_collection.find({"topics": topic})
+        for doc in match_list:
