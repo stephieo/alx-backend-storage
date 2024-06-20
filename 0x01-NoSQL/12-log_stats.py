bc65e763a4a3c06cfe9ec7be1fe3ccf71b83b4c3
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2024-06-20 05:08:31 +0100

    fix: include conditional execution

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,19 +3,20 @@
 from pymongo import MongoClient
 
 
-client = MongoClient()
-logs = client.logs
-nginx = logs.nginx
+if __name__ == "__main__":
+    client = MongoClient()
+    logs = client.logs
+    nginx = logs.nginx
 
-doc_count = nginx.count_documents({})
-print(f'{doc_count} logs')
+    doc_count = nginx.count_documents({})
+    print(f'{doc_count} logs')
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-print('Methods:')
-for method in methods:
-    count = nginx.count_documents({"method": method})
-    print(f'    method {method}: {count}')
+    print('Methods:')
+    for method in methods:
+        count = nginx.count_documents({"method": method})
+        print(f'    method {method}: {count}')
 
-count = nginx.count_documents({"method": "GET", "path": "/status"})
-print(f'{count} status check')
+    count = nginx.count_documents({"method": "GET", "path": "/status"})
+    print(f'{count} status check')
